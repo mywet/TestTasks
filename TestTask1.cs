361463be367a9117ed36b5078ea06254bffa5e23
mywet <93406336+mywet@users.noreply.github.com>
2021-11-07 22:44:44 +0200

    Тестовое задание №1

---
+++ TestTask1.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject camerFollow, spectatorCamera;
+
+    [SerializeField]
+    private Text damagetext;
+
+    [SerializeField]
+    private Slider healthSlider;
+
+
+    private const int MAXHEALTH = 100;
+
+    private int currentHealth, damage;
+
+    void Start()
+    {
+        currentHealth = MAXHEALTH;
+    }
+    void Update()
+    {
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0)
+            Isdead();
+        else
+            damagetext.text = currentHealth.ToString();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+            currentHealth -= damage;
+        /*Если у врага есть компонент Enemy
+        if (other.gameObject.GetComponent<Enemy>() != null)
+            currentHealth -= damage;
+        */
+    }
+    private bool Isdead()
+    {
+        CarDestroying();
+        SetSpectatorMode();
+        return true;
+    }
+    private void CarDestroying()
+    {
+        Destroy(gameObject);
+    }
+    private void SetSpectatorMode()
+    {
+        spectatorCamera.SetActive(true);
+        camerFollow.SetActive(false);
+    }
+}
